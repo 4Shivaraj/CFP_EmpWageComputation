26c3e261d41ae67b1b2bc63121418575ba84aead
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-08-29 21:50:09 +0530

    [Shivaraj] Added checking maximum working days and hours problem

--- emp_wage_compuations.py
+++ emp_wage_compuations.py
@@ -1,22 +1,24 @@
 import random
 from emp_log import get_logger
 
-lg = get_logger("(monthly_wage)", file_name="emp_log.log")
+lg = get_logger("(max_hours_days)", file_name="emp_log.log")
 
 
 class Employee:
 
     def calculate_emp_wage(self):
         """
-        in this function added checking monthly wage of an employee.
+        in this function added checking maximum working days and hours of an employee.
         :return: None.
         """
         try:
             emp_wage_hour = 20
             max_working_days = 20
+            max_working_hours = 100
             emp_wage_per_month = 0
             emp_working_days = 0
-            while emp_working_days < max_working_days:
+            emp_working_hours = 0
+            while emp_working_days < max_working_days and emp_working_hours < max_working_hours:
                 emp_check = random.randint(0, 2)
                 lg.info(emp_check)
                 emp_dict = {1: 8, 2: 4, 0: 0}
@@ -25,8 +27,11 @@ class Employee:
                 lg.debug(" daily wage of an employee {}".format(daily_wage) if daily_wage != 0 else
                          "employee is absent")
                 emp_wage_per_month += daily_wage
+                emp_working_hours += working_hour
                 emp_working_days += 1
+            lg.info("Days the employees have worked: {} ".format(emp_working_days))
             lg.debug("Employee wage for month: {}".format(emp_wage_per_month))
+            lg.debug("Hours the employees have worked: {} ".format(emp_working_hours))
             return
 
         except Exception as e:
