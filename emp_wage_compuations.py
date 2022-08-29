b72b9b05cf872a7988188248d986195769ad2128
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-08-29 21:33:01 +0530

    [Shivaraj] Added checking employee monthly wage problem

--- emp_wage_compuations.py
+++ emp_wage_compuations.py
@@ -1,25 +1,33 @@
 import random
 from emp_log import get_logger
 
-lg = get_logger("(part_time_wage)", file_name="emp_log.log")
+lg = get_logger("(monthly_wage)", file_name="emp_log.log")
 
 
 class Employee:
 
     def calculate_emp_wage(self):
         """
-        in this function added checking part-time wage of an employee.
+        in this function added checking monthly wage of an employee.
         :return: None.
         """
         try:
             emp_wage_hour = 20
-            emp_check = random.randint(0, 2)
-            lg.info(emp_check)
-            emp_dict = {1: 8, 2: 4, 0: 0}
-            working_hour = emp_dict.get(emp_check)
-            daily_wage = working_hour * emp_wage_hour
-            lg.debug(" daily wage of an employee {}".format(daily_wage) if daily_wage != 0 else
-                     "employee is absent")
+            max_working_days = 20
+            emp_wage_per_month = 0
+            emp_working_days = 0
+            while emp_working_days < max_working_days:
+                emp_check = random.randint(0, 2)
+                lg.info(emp_check)
+                emp_dict = {1: 8, 2: 4, 0: 0}
+                working_hour = emp_dict.get(emp_check)
+                daily_wage = working_hour * emp_wage_hour
+                lg.debug(" daily wage of an employee {}".format(daily_wage) if daily_wage != 0 else
+                         "employee is absent")
+                emp_wage_per_month += daily_wage
+                emp_working_days += 1
+            lg.debug("Employee wage for month: {}".format(emp_wage_per_month))
+            return
 
         except Exception as e:
             lg.error(e)
