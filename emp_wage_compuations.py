4b1984499f380f27bf525a5f274d8d0e42671e68
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-08-30 14:27:36 +0530

    [Shivaraj] Added managing multiple companies with employee details

--- emp_wage_compuations.py
+++ emp_wage_compuations.py
@@ -1,15 +1,33 @@
 import random
 from emp_log import get_logger
 
-lg = get_logger("(MultipleCompany)", file_name="emp_log.log")
+lg = get_logger("(WageForMultipleCompany)", file_name="emp_log.log")
+
+
+class Company:
+    def __init__(self, company_name, emp_data):
+        self.company_name = company_name
+        self.emp_data = emp_data
+
+    def company_dict(self):
+        company_wage_data = {}
+        company_wage_data.update({self.company_name: self.emp_data})
+        return company_wage_data
 
 
 class Employee:
-    def __init__(self, company_name, emp_wage_hour, max_working_days, max_working_hours):
+    def __init__(self, emp_name, salary_details_dict, company_name,
+                 emp_wage_hour, max_working_days, max_working_hours):
+        self.emp_name = emp_name
+        self.salary_details_dict = salary_details_dict
         self.company_name = company_name
         self.emp_wage_hour = emp_wage_hour
         self.max_working_days = max_working_days
         self.max_working_hours = max_working_hours
+
+    def salary_dict(self, emp_name, emp_wage):
+        self.salary_details_dict.update({emp_name: emp_wage})
+        return self.salary_details_dict
 
     def calculate_emp_wage(self):
         """
@@ -32,20 +50,26 @@ class Employee:
                 emp_wage_per_month += daily_wage
                 emp_working_hours += working_hour
                 emp_working_days += 1
+            salary_data = self.salary_dict(self.emp_name, emp_wage_per_month)
+            lg.info("salary_data: {}".format(salary_data))
             lg.info("Name of the company: {}".format(self.company_name))
             lg.info("Days the employees have worked: {} ".format(emp_working_days))
             lg.debug("Employee wage for month: {}".format(emp_wage_per_month))
             lg.debug("Hours the employees have worked: {} ".format(emp_working_hours))
-            return
+            return salary_data
 
         except Exception as e:
             lg.error(e)
 
 
 if __name__ == '__main__':
-
-    emp1_wage = Employee("FlipKart", 20, 20, 100)
-    emp1_wage.calculate_emp_wage()
-    emp2_wage = Employee("Amazon", 30, 19, 110)
-    emp2_wage.calculate_emp_wage()
-
+    flipkart_emp1_wage = Employee("Shivaraj", {}, "FlipKart", 30, 20, 100).calculate_emp_wage()
+    flipkart_emp2_wage = Employee("Cheluvesh", {}, "FlipKart", 20, 25, 105).calculate_emp_wage()
+    amazon_emp1_wage = Employee("gayathri", {}, "Amazon", 28, 18, 95).calculate_emp_wage()
+    amazon_emp2_wage = Employee("raksha", {}, "Amazon", 35, 15, 90).calculate_emp_wage()
+    flipkart_emp1_wage.update(flipkart_emp2_wage)
+    amazon_emp1_wage.update(amazon_emp2_wage)
+    flipkart = Company("FlipKart", flipkart_emp1_wage).company_dict()
+    amazon = Company("amazon", amazon_emp1_wage).company_dict()
+    lg.debug(flipkart)
+    lg.debug(amazon)
